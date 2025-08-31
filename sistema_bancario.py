486de40c94b58b2e79032c00a624f37f81bd286f
Rayan Alcantara <169080529+rayantrindad13@users.noreply.github.com>
2025-08-31 15:36:15 -0300

    update def usuarios

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -54,37 +54,35 @@ def exibir_extrato(saldo, /, *, extrato):
     print(f"\nSaldo: R$ {saldo:.2f}")   
     print("=========================================")
 
-def criar_usuario(usuario):
-    cpf = input("Digite o CPF (somente números): ")
-    usuario = filtrar_usuario(cpf, usuario)
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente número): ")
+    usuario = filtrar_usuario(cpf, usuarios)
 
-    if not usuario:
-        print("Usuário não encontrado.")
+    if usuario:
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
         return
 
-    nome = input("Digite o nome completo: ")
-    data_nascimento = input("Digite a data de nascimento (dd-mm-aaaa): ")
-    endereco = input("Digite o endereço completo: ")
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
 
-    usuario.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
-    print("===Usuário criado com sucesso!===")
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com sucesso! ===")
 
 def filtrar_usuario(cpf, usuarios):
     usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
     return usuarios_filtrados[0] if usuarios_filtrados else None
 
 def criar_conta(agencia, numero_conta, usuarios):
-    cpf = input("Digite o CPF do usuário: ")
+    cpf = input("Informe o CPF do usuário: ")
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n===Conta criada com sucesso!===")
-        return {
-            "agencia": agencia,
-            "numero_conta": numero_conta,
-            "usuario": usuario
-        }
-    print("\n@@@Usuário não encontrado, fluxo de criação de conta encerrado!@@@")
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
 
 def listar_contas(contas):
     for conta in contas:
